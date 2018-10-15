5b269e02966335caabb0b4bd99a28894a0956c52
SwubTub <charliecarr03@gmail.com>
2018-10-14 22:04:52 -0400

    Added enemy and weapon / item rooms

--- Tutorial/tiles.py
+++ Tutorial/tiles.py
@@ -38,4 +38,71 @@ class EnemyRoom(Maptile):
 class EmptyCavePath(Maptile):
     def intro_text(self):
         return """
-        Another 
+        Another empty, bland part of the cave...
+        """
+    def modify_player(self, player):
+        #Room has no action on player#
+        pass
+class GoblinRoom(EnemyRoom):
+    def __init__(self, x, y, enemy):
+        super().__init__(x, y, enemies.Goblin())
+
+    def intro_text(self):
+        if self.enemy.is_alive():
+            return """
+            A goblin jumps out from the shadows right in front of you!
+            """
+        else:
+            return """
+            A rotting goblin corpse is laying in front of you.
+            """
+class SkeletonRoom(EnemyRoom):
+    def __init__(self, x, y, enemy):
+        super().__init__(x, y, enemies.Skeleton())
+
+    def intro_text(self):
+        if self.enemy.is_alive():
+            return """
+            A skeleton jumps you and is ready to kill you!
+            """
+        else:
+            return """
+            A pile of bones and a sword are sitting in the corner of the room.
+            """
+class OrcRoom(EnemyRoom):
+    def __init__(self, x, y, enemy):
+        super().__init__(x, y, enemies.Orc())
+
+    def intro_text(self):
+        if self.enemy.is_alive():
+            return """
+            A large Orc is swinging his club, hungry for dinner!
+            """
+        else:
+            return """
+            A club is leaning against the wall, looks like the Orc is roaming.
+            """
+class FindMopRoom(LootRoom):
+    def __init__(self, x, y, item):
+        super().__init__(x, y, items.Mop())
+
+    def intro_text(self):
+        return """
+        You catch a faint whiff of cleaning supplies. In the corner is a clean, shiny mop! You pick it up and add it to your inventory.
+        """
+class FindStickRoom(LootRoom):
+    def __init__(self, x, y, item):
+        super().__init__(x, y, items.Stick())
+
+    def intro_text(self):
+        return """
+        A STICK! YAY! You proudly pick it up and carry along on your jolly travels.
+        """
+class FindSwordRoom(LootRoom):
+    def __init__(self, x, y, item):
+        super().__init__(x, y, items.Sword())
+
+    def intro_text(self):
+        return """
+        A shiny sword is lying in the middle of the room, you pick it up and add it to your inventory.
+        """
