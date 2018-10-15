e2c86946687800e55d61867b95ce330b00b68950
SwubTub <charliecarr03@gmail.com>
2018-10-14 21:22:36 -0400

    Items and Tiles

--- Tutorial/tiles.py
+++ Tutorial/tiles.py
@@ -34,4 +34,3 @@ class EnemyRoom(Maptile):
         if self.enemy.is_alive():
             the_player.hp = the_player.hp - self.enemy.damage
             print("Enemy does {} damage. You have {} HP remaining.".format(self.enemy.damage, the_player.hp))
-        
