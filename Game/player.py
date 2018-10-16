c207e049be9a1bacd4985bd7ed8886cea686e06b
SwubTub <charliecarr03@gmail.com>
2018-10-15 21:01:53 -0400

    more actions

--- Game/player.py
+++ Game/player.py
@@ -1,5 +1,4 @@
-import items, enemies, actions, world
-sys.path.append("..") # Adds higher directory to python modules path.
+import items, tiles, actions, world
 
 class Player():
     def __init__(self):
@@ -47,3 +46,59 @@ class Player():
             print('You killed {}!'.format(enemy.name))
         else:
             print('{} HP is []!'.format(enemy.name, enemy.hp))
+
+def adjacent_moves(self):
+    """Returns all moves for adjacent tiles."""
+    moves = []
+    if world.tile_exists(self.x + 1, self.y):
+        moves.append(actions.MoveEast())
+    if world.tile_exists(self.x - 1, self.y):
+        moves.append(actions.MoveWest())
+    if world.tile_exists(self.x, self.y - 1):
+        moves.append(actions.MoveNorth())
+    if world.tile_exists(self.x, self.y + 1):
+        moves.append(actions.MoveSouth())
+    return moves
+
+def available_actions(self):
+    """Returns all available actions in this room."""
+    moves = self.adjacent_moves()
+    moves.append(actions.ViewInventory())
+
+    return moves
+
+def do_action(self, action, **kwargs):
+    action_method = getattr(self, action.method.__name__)
+    if action_method:
+        action_method(**kwargs)
+
+import random #Note the new import!
+import items, world
+ 
+class Player:
+ 
+    def flee(self, tile):
+        """Moves the player randomly to an adjacent tile"""
+        available_moves = tile.adjacent_moves()
+        r = random.randint(0, len(available_moves) - 1)
+        self.do_action(available_moves[r])
+
+class Flee(Action):
+    def __init__(self, tile):
+        super().__init__(method=Player.flee, name="Flee", hotkey='f', tile=tile)
+
+class EnemyRoom(MapTile):
+    def __init__(self, x, y, enemy):
+        self.enemy = enemy
+        super().__init__(x, y)
+ 
+    def modify_player(self, the_player):
+        if self.enemy.is_alive():
+            the_player.hp = the_player.hp - self.enemy.damage
+            print("Enemy does {} damage. You have {} HP remaining.".format(self.enemy.damage, the_player.hp))
+ 
+    def available_actions(self):
+        if self.enemy.is_alive():
+            return [actions.Flee(tile=self), actions.Attack(enemy=self.enemy)]
+        else:
+            return self.adjacent_moves()
